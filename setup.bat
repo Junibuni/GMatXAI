512aa3f1d2c69f1d4fce175b440d926fec600867
Junibuni <rich4103@naver.com>
2025-05-23 16:58:43 +0900

    modify setup

--- setup.bat
+++ setup.bat
@@ -4,16 +4,17 @@ echo Creating virtual environment...
 python -m venv venv
 call .\venv\Scripts\activate
 
-pip install --upgrade pip wheel setuptools
+python -m pip install --upgrade pip
+pip install typing-extensions==4.12.2 --index-url https://pypi.org/simple
 
 echo Installing PyTorch (%CUDA_VERSION%)...
-pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu126
 
 echo Installing torch_geometric...
 pip install torch_geometric
 
 echo Installing PyG dependencies...
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.6.0+cu118.html
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.6.0+cu126.html
 
 echo Installing from requirements.txt...
 pip install -r requirements.txt
